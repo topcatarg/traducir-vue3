bd2626515907b8ec816abc81ff4ce2dce2d68685
Gonzalo <topcat_arg@yahoo.com>
2018-08-14 16:49:34 -0300

    Change header file. routing enabled (#3)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
@@ -5,6 +9,7 @@ import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
