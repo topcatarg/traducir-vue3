b3a541a2029262b108f348944acb50a8d60d8877
Gonzalo <topcat_arg@yahoo.com>
2018-08-14 13:18:00 -0300

    Router and view mocks created (#2)

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Main from './views/Main.vue';
+import Suggestions from './views/Suggestions.vue';
+import Users from './views/Users.vue';
 
 Vue.use(Router);
 
@@ -8,16 +11,36 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Main',
+      component: Main
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/Users',
+      name: 'Users',
+      component: Users
     },
-  ],
+    {
+      path: '/Suggestions',
+      name: 'Suggestions',
+      component: Suggestions
+    }
+  ]
 });
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: Home,
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (about.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+//     },
+//   ],
+// });
---
+++ src/views/Suggestions.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      suggestions
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'suggestions'
+});
+</script>
+
