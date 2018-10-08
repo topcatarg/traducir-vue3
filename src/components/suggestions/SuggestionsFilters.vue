418c2a65f962f2727ae155bcbd0c3fddb9a47237
gbianchi <gbianchi@afip.gob.ar>
2018-10-08 12:43:11 -0300

    Finished user screen

--- src/components/suggestions/SuggestionsFilters.vue
+++ src/components/suggestions/SuggestionsFilters.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container mt-3">
         <b-button-group>
-            <b-button class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id">All</b-button>
-            <b-button class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=1'">Created</b-button>
-            <b-button class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=2'">Approved by a trusted user</b-button>
-            <b-button class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=3'">Approved by a reviewer</b-button>
-            <b-button class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=4'">Rejected</b-button>
-            <b-button class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=5'">Deleted by owner</b-button>
-            <b-button class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=6'">Dismissed by other string</b-button>
+            <b-button exact class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id">All</b-button>
+            <b-button exact class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=1'">Created</b-button>
+            <b-button exact class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=2'">Approved by a trusted user</b-button>
+            <b-button exact class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=3'">Approved by a reviewer</b-button>
+            <b-button exact class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=4'">Rejected</b-button>
+            <b-button exact class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=5'">Deleted by owner</b-button>
+            <b-button exact class="mx-1" variant="outline-info" :to="'/Suggestions/' + User.id + '?state=6'">Dismissed by other string</b-button>
         </b-button-group>
     </div>
 </template>
